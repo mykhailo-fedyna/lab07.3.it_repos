29bd2010897b6ee3b2f1d7e3d4dae1f710269fc4
misha-fedyna <mihajlofedina5@gmail.com>
2024-10-12 15:06:06 +0300

    first commit

---
+++ Lab7.3(it).cpp
@@ -0,0 +1,76 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+void InputMatrix(double** matrix, const int size);
+void PrintMatrix(double** matrix, const int size);
+void ArrangeMaxOnDiagonal(double** matrix, const int size);
+int FindFirstRowWithoutPositive(double** matrix, const int size);
+int main() {
+    int n;
+    cout << "Enter the size of the square matrix (n x n): ";
+    cin >> n;
+    double** matrix = new double* [n];
+    for (int i = 0; i < n; i++)
+        matrix[i] = new double[n];
+    InputMatrix(matrix, n);
+    cout << "Initial matrix:" << endl;
+    PrintMatrix(matrix, n);
+    ArrangeMaxOnDiagonal(matrix, n);
+    cout << "Matrix after rearranging max elements on the diagonal:" << endl;
+    PrintMatrix(matrix, n);
+    int firstRowWithoutPositive = FindFirstRowWithoutPositive(matrix, n);
+    if (firstRowWithoutPositive != -1)
+        cout << "First row without positive elements: " << firstRowWithoutPositive << endl;
+    else
+        cout << "All rows contain positive elements." << endl;
+    for (int i = 0; i < n; i++)
+        delete[] matrix[i];
+    delete[] matrix;
+    return 0;
+}
+void InputMatrix(double** matrix, const int size) {
+    cout << "Enter the elements of the matrix:" << endl;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << "matrix[" << i << "][" << j << "] = ";
+            cin >> matrix[i][j];
+        }
+    }
+}
+void PrintMatrix(double** matrix, const int size) {
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            cout << setw(8) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+void ArrangeMaxOnDiagonal(double** matrix, const int size) {
+    for (int i = 0; i < size; i++) {
+        int maxRow = i, maxCol = i;
+        for (int row = i; row < size; row++) {
+            for (int col = 0; col < size; col++) {
+                if (matrix[row][col] > matrix[maxRow][maxCol]) {
+                    maxRow = row;
+                    maxCol = col;
+                }
+            }
+        }
+        swap(matrix[i][i], matrix[maxRow][maxCol]);
+    }
+}
+int FindFirstRowWithoutPositive(double** matrix, const int size) {
+    for (int i = 0; i < size; i++) {
+        bool hasPositive = false;
+        for (int j = 0; j < size; j++) {
+            if (matrix[i][j] > 0) {
+                hasPositive = true;
+                break;
+            }
+        }
+        if (!hasPositive)
+            return i;
+    }
+    return -1;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,28 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab7.3(it).cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+int FindFirstRowWithoutPositive(double** matrix, const int size);
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		TEST_METHOD(TestFindFirstRowWithoutPositive)
+		{
+			int size = 3;
+			double** matrix = new double* [size];
+			for (int i = 0; i < size; i++)
+				matrix[i] = new double[size];
+			matrix[0][0] = -1; matrix[0][1] = -2; matrix[0][2] = -3;
+			matrix[1][0] = 4;  matrix[1][1] = 5;  matrix[1][2] = 6;
+			matrix[2][0] = -7; matrix[2][1] = -8; matrix[2][2] = 9;
+			int rowWithoutPositive = FindFirstRowWithoutPositive(matrix, size);
+			Assert::AreEqual(0, rowWithoutPositive);
+			for (int i = 0; i < size; i++)
+				delete[] matrix[i];
+			delete[] matrix;
+		}
+	};
+}
+
